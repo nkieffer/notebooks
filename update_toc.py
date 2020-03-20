9b7db495325fe7912010f433b53ee2e2297839d9
Nathan Kieffer <nathankieffer@gmail.com>
2020-03-20 17:14:46 +0700

    work on while loops

--- update_toc.py
+++ update_toc.py
@@ -1,15 +1,52 @@
 #!/usr/bin/env python
 import pathlib
 from re import split
-
-notebooks = sorted(pathlib.Path('.').glob('{3}[0-9]*.ipynb'))
-
-with open('ToC.md', 'w') as toc:
+notebooks = sorted(list(pathlib.Path('.').glob('[0-9][0-9][0-9]*.ipynb')))
+print(notebooks)
+with open('000toc.ipynb', 'w') as toc:
+    contents = []
     for f in notebooks:
         title_idx = split('[0-9]', f.name).count('')
         name = f.name[title_idx:]
         unit = f.name[:title_idx]
         link_text = name.replace(f.suffix, '').replace('_', ' ')
-        toc.write(f"* [Unit {unit}: {link_text}]({name})\n")
-    
-
+        link = f'"* [Unit {unit}: {link_text}]({f.name})\\n"'
+        contents.append(link)
+    print(contents)
+    template = f"""
+{{
+ "cells": [
+  {{
+   "cell_type": "markdown",
+   "metadata": {{}},
+   "source": [
+       "# Table of Contents\\n",
+    {', '.join(contents)}
+    ]
+  }}
+ ],
+ "metadata": {{
+  "kernelspec": {{
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  }},
+  "language_info": {{
+   "codemirror_mode": {{
+    "name": "ipython",
+    "version": 3
+   }},
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.6.9"
+  }}
+ }},
+ "nbformat": 4,
+ "nbformat_minor": 4
+}}
+"""
+    print(template)
+    toc.write(template)
